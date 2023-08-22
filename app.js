955346f67ebc0f666148fa8b1eaad1875eaa7133
Phuc Tran <pnt216@yahoo.com>
2023-08-22 00:28:10 -0700

    Your commit message here

--- app.js
+++ app.js
@@ -159,7 +159,7 @@ app.use((err, req, res, next) => {
     res.status(statusCode).render('error', { err })
 })
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Serving on port ${port}`)
 })
